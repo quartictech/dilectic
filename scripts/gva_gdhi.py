9bf6ec91b4cdefad31df1bfa5ea454bea984e0b0
Arlo Bryer <arlo@quartic.io>
2016-08-14 16:01:01 +0100

    Collect everything in a big dictionary

--- scripts/gva_gdhi.py
+++ scripts/gva_gdhi.py
@@ -1,23 +1,43 @@
 from xlrd import open_workbook
 wb = open_workbook('/Users/arlogb/quartic/dilectic/data/london_datastore/GVA-GDHI-nuts3-regions-uk.xls')
 
-of_interest = ['GVA', 'GVA Per Head', 'per head Indicies',
-                'Headline gross disposible house', 'GDHI per Head']
 
 def process_gva_sheet(s):
     years = s.row(0)[3:]
-    print((years[0].value))
-    print(len(years))
+    insert = []
     for row in range(s.nrows):
         if str(s.cell(row,1).value)[:3] == "UKI":
             for n, v in enumerate(years):
-                insert = []
-                insert.append(str(s.cell(row,1).value))
-                insert.append(int(v.value))
-                insert.append(str(s.cell(row,2).value))
-                insert.append(int(s.cell(row,n+3).value))
-                print(','.join(str(v) for v in insert))
+                r = []
+                r.append(str(s.cell(row,1).value))
+                r.append(int(v.value))
+                r.append(str(s.cell(row,2).value))
+                r.append(int(s.cell(row,n+3).value))
+                insert.append(r)
+    return insert
 
-for s in wb.sheets():
-    if s.name == 'GVA':
-        process_gva_sheet(s)
+def collector(insert, d=None):
+    if d is None:
+        d = {}
+    else:
+        for i in insert:
+            key = ''.join((i[0], str(i[1])))
+            if key in d.keys():
+                d[key].append(i[-1])
+            else:
+                d[key] = [i[-1]]
+    return d
+
+def process_workbook(wb):
+    of_interest = ['GVA', 'GVA Per Head', 'per head Indices',
+                'Headline gross disposable house', 'GDHI per Head']
+    headers = ['UKI', 'Year', 'Area']
+    table = {}
+    for s in wb.sheets():
+        if s.name in of_interest:
+            headers.append(s.name)
+            table = collector(process_gva_sheet(s), table)
+    table['headers'] = headers
+    return table
+
+print(process_workbook(wb))
