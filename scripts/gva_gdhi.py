67d2aa2259704944103bd5481827a525dd4e8e0a
Arlo Bryer <arlo@quartic.io>
2016-08-14 14:48:36 +0100

    Script produces correct inserts for pg.

--- scripts/gva_gdhi.py
+++ scripts/gva_gdhi.py
@@ -4,11 +4,20 @@ wb = open_workbook('/Users/arlogb/quartic/dilectic/data/london_datastore/GVA-GDH
 of_interest = ['GVA', 'GVA Per Head', 'per head Indicies',
                 'Headline gross disposible house', 'GDHI per Head']
 
+def process_gva_sheet(s):
+    years = s.row(0)[3:]
+    print((years[0].value))
+    print(len(years))
+    for row in range(s.nrows):
+        if str(s.cell(row,1).value)[:3] == "UKI":
+            for n, v in enumerate(years):
+                insert = []
+                insert.append(str(s.cell(row,1).value))
+                insert.append(int(v.value))
+                insert.append(str(s.cell(row,2).value))
+                insert.append(int(s.cell(row,n+3).value))
+                print(','.join(str(v) for v in insert))
 
 for s in wb.sheets():
-    if s.name in of_interest:
-        for row in range(s.nrows):
-            values = []
-            for col in range(s.ncols):
-                values.append(str(s.cell(row,col).value))
-            print(','.join(values))
+    if s.name == 'GVA':
+        process_gva_sheet(s)
