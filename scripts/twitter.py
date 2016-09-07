96d4902b144e31e03c96f1b2041f968af13ae087
Arlo Bryer <arlobryer@gmail.com>
2016-09-07 16:57:48 +0100

    Some tidying up.

--- scripts/twitter.py
+++ scripts/twitter.py
@@ -7,50 +7,66 @@ import sys
 import argparse
 from metrology import Metrology
 
+def setup_stream():
+
+    config = configparser.ConfigParser()
+    config.read('twitter.conf')
+
+    query = {'locations' : config['query']['locations'],
+           'track' : config['query']['track']}
+
+    s = requests.Session()
+    auth = OAuth1(
+        config['api']['appkey'], config['api']['appsecret'],
+        config['api']['useroauthtoken'], config['api']['useroauthtokensecret'])
+
+    r = requests.post('https://stream.twitter.com/1.1/statuses/filter.json',
+        data=query,
+        stream=True,
+        auth=auth)
+
+    return r
+
+def get_tweets(outfile=None):
+    r = setup_stream()
+    count = 0
+    meter = Metrology.meter('tweets')
+    if outfile:
+        with open(outfile, 'a') as f:
+            for line in r.iter_lines():
+                if meter.count % 10 == 0:
+                    print("Wrote", meter.count, "tweets. Rate=", meter.one_minute_rate * 60 * 60 * 24,"/day")
+                if os.path.getsize(outfile)/10**9 < 500:
+                  try:
+                    decoded = line.decode('utf-8')
+                    f.write(decoded + "\n")
+                    meter.mark()
+                    #print(json.loads(decoded))
+                    # tweet = json.loads(line.decode('utf-8'))
+                  except Exception as e:
+                    print(e)
+                    print(line)
+                else:
+                  sys.exit()
+    else:
+        for line in r.iter_lines():
+            if meter.count % 10 == 0:
+                print("Wrote", meter.count, "tweets. Rate=", meter.one_minute_rate * 60 * 60 * 24,"/day")
+            if os.path.getsize(outfile)/10**9 < 500:
+              try:
+                decoded = line.decode('utf-8')
+                f.write(decoded + "\n")
+                print(json.loads(decoded))
+              except Exception as e:
+                print(e)
+                print(line)
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("-f", "--file", help="Specify file location", nargs='?',
-                    type=argparse.FileType('a'),
                     action="store")
   args = parser.parse_args()
   outfile = args.file
-  print(outfile)
-
-  config = configparser.ConfigParser()
-  config.read('twitter.conf')
-  
-  query = {'locations' : config['query']['locations'],
-           'track' : config['query']['track']}
-
-  s = requests.Session()
-  auth = OAuth1(
-    config['api']['appkey'], config['api']['appsecret'],
-    config['api']['useroauthtoken'], config['api']['useroauthtokensecret'])
-
-  count = 0
-  meter = Metrology.meter('tweets')
-  while True:
-    r = requests.post('https://stream.twitter.com/1.1/statuses/filter.json',
-      data=query,
-      stream=True,
-      auth=auth)
-    with outfile as file:
-      for line in r.iter_lines():
-        if meter.count % 10 == 0:
-            print("Wrote", meter.count, "tweets. Rate=", meter.one_minute_rate * 60 * 60 * 24,"/day")
-        if os.path.getsize(outfile.name)/10**9 < 500:
-          try:
-            decoded = line.decode('utf-8')
-            file.write(decoded + "\n")
-            meter.mark()
-            #print(json.loads(decoded))
-            # tweet = json.loads(line.decode('utf-8'))
-          except Exception as e:
-            print(e)
-            print(line)
-        else:
-          sys.exit()
-
-
+  get_tweets(outfile)
 #dict_keys(['place', 'source', 'contributors', 'retweeted', 'lang', 'retweet_count', 'text', 'id', 'possibly_sensitive', 'id_str', 'filter_level', 'favorited', 'coordinates', 'truncated', 'in_reply_to_user_id_str', 'is_quote_status', 'in_reply_to_user_id', 'in_reply_to_status_id', 'timestamp_ms', 'entities', 'created_at', 'user', 'in_reply_to_status_id_str', 'favorite_count', 'geo', 'in_reply_to_screen_name'])
-
