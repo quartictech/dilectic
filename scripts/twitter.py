0ddc9dc8e988629004d483383fc11a25519e106d
Arlo Bryer <arlo@quartic.io>
2016-09-11 21:49:21 +0100

    Add a catch to keep the stream alive.

--- scripts/twitter.py
+++ scripts/twitter.py
@@ -30,20 +30,21 @@ def setup_stream():
 def read_stream(request, outfile=None):
     if outfile:
         with open(outfile, 'a') as f:
-            for line in request.iter_lines():
-                meter = Metrology.meter('tweets')
-                if meter.count % 10 == 0:
-                    print("Wrote", meter.count, "tweets. Rate=", meter.one_minute_rate * 60 * 60 * 24,"/day")
-                if os.path.getsize(outfile)/10**9 < 500:
-                    try:
-                        decoded = line.decode('utf-8')
-                        f.write(decoded + "\n")
-                        meter.mark()
-                    except Exception as e:
-                        print(e)
-                        print(line)
-                else:
-                    sys.exit()
+            while True:#to avoid the stream hanging up
+                for line in request.iter_lines():
+                    meter = Metrology.meter('tweets')
+                    if meter.count % 10 == 0:
+                        print("Wrote", meter.count, "tweets. Rate=", meter.one_minute_rate * 60 * 60 * 24,"/day")
+                    if os.path.getsize(outfile)/10**9 < 500:
+                        try:
+                            decoded = line.decode('utf-8')
+                            f.write(decoded + "\n")
+                            meter.mark()
+                        except Exception as e:
+                            print(e)
+                            print(line)
+                    else:
+                        sys.exit()
     else:
         try:
             decoded = line.decode('utf-8')
@@ -53,7 +54,6 @@ def read_stream(request, outfile=None):
             print(line)
 
 def get_tweets(outfile=None):
-    print(outfile)
     r = setup_stream()
     read_stream(r, outfile)
 
