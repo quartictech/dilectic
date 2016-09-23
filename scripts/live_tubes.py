9671725edac6b5176194d997cf641c6bf7d66f30
Arlo Bryer <arlo@quartic.io>
2016-09-23 12:38:31 +0100

    Build a map of the station locations.

--- scripts/live_tubes.py
+++ scripts/live_tubes.py
@@ -1,10 +1,23 @@
 import requests
 import time
+import datetime
+import calendar
 import configparser
-import pprint
+from pprint import pprint
+from geojson import Point
+import psycopg2
+import sys
 
+conn = psycopg2.connect("dbname=postgres user=postgres")
+cur = conn.cursor()
+cur.execute("SELECT * FROM naptan;")
 config = configparser.ConfigParser()
 config.read('tfl.conf')
+
+def get_naptan_locs(db_cursor, naptan):
+    query = db_cursor.mogfiry("SELECT * FROM naptan WHERE stopareacode IN %s;",
+                        naptan)
+    pass
 
 app_id = config['DEFAULT']['AppID']
 app_key = config['DEFAULT']['AppKey']
@@ -18,9 +31,21 @@ lines = ['bakerloo', 'central', 'circle', 'district', 'hammersmith-city',
             'waterloo-city']
 lines_data = s.get(api_root + '/Line/' + ','.join(lines), params = {'app_id' : app_id, 'app_key' : app_key})
 
+def get_stop_locations():
+    stations  = {}
+    for l in lines:
+        stop_points = s.get(api_root + '/Line/' + l + '/StopPoints').json()
+        for stop in stop_points:
+            stations[stop['stationNaptan']]  = Point((stop['lat'], stop['lon']))
+    return stations
+
+stations = get_stop_locations()
+assert len(stations.keys()) == 270 #check that we have all the stations
+sys.exit()
 def update_locs(loc_train):
     resp = s.get(api_root + '/Line/' + ','.join(lines) + '/Arrivals')
     arrivals = resp.json()
+    pprint.pprint(arrivals)
     for a in arrivals:
         if 'naptanId' in a.keys():
             current_loc = a['naptanId']
@@ -31,8 +56,10 @@ loc_train = {}#for every station, which train
 new_loc_train = {}
 while True:
     new_loc_train = update_locs(new_loc_train)
+    print(new_loc_train)
+    break
     for k, v in new_loc_train.items():
         if k in loc_train.keys() and v != loc_train[k]:
-            print(new_loc_train[k])
+            print(k)
     loc_train = new_loc_train.copy()
     time.sleep(9)
