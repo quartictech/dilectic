43a0a2e599f414a617a1ae76a0e90be22bbf565b
Alex Sparrow <alex@quartic.io>
2016-10-03 17:58:32 +0100

    Finish dockerizing dilectic integrations

---
+++ integration/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/config.sh
+
+docker_run() {
+ echo "Running $1 in docker"
+ sudo docker run --net="host" -v $DIR:/home/integration -v $DERIVED_DIR:/home/data/derived -v $RAW_DIR:/home/data/raw data-integration /home/integration/$1
+}
+
+#docker_run 0-from-raw.sh
+#docker_run 1-dump-osm-to-sql.sh
+docker_run 2-import-postgres.sh
