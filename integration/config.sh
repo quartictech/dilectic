62236875f2d10a20168c2d0c08eb50eb8892d122
Alex Sparrow <alex@quartic.io>
2016-10-09 18:18:32 +0100

    README and nicer config

--- integration/config.sh
+++ integration/config.sh
@@ -1,7 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export RAW_DIR=$DIR/../data2/raw
-export DERIVED_DIR=$DIR/../data2/derived
+export RAW_DIR=$DIR/../data/raw
+export DERIVED_DIR=$DIR/../data/derived
 
 export PGPASSWORD=dilectic
 echo $PGPASSWORD
