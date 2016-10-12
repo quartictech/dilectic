15aba05f23f2587842626b895f532943560ce421
Oliver Charlesworth <oliver@quartic.io>
2016-10-12 19:42:03 +0100

    Fix hostnames

--- integration/1-dump-osm-to-sql.sh
+++ integration/1-dump-osm-to-sql.sh
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/config.sh
 
-osm2pgsql $INPUT/greater-london-latest.osm.pbf -d postgres -U postgres -P 5432 -H localhost 
-psql -d postgres -U postgres -a -f $DIR/scripts/rename_columns.sql -h localhost
+osm2pgsql $INPUT/greater-london-latest.osm.pbf -d postgres -U postgres -P 5432 -H mdillon__postgis
+psql -d postgres -U postgres -a -f $DIR/scripts/rename_columns.sql -h mdillon__postgis
