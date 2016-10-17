ecd3fecebd5ad50dbe886b7e6029d36d2fc29cb8
Arlo Bryer <arlo@quartic.io>
2016-10-17 17:38:08 +0100

    Sorted bus stops.

--- integration/scripts/tfl_lines.py
+++ integration/scripts/tfl_lines.py
@@ -56,7 +56,7 @@ def fetch_arrival_predictions(line):
             bus_arrivals[bus['vehicleId']].append(bus)
         else:
             bus_arrivals[bus['vehicleId']] = [bus]
-    return bus_arrivals
+    return {bus: sorted(arrivals, key=lambda k: k['timeToStation']) for bus, arrivals in bus_arrivals.items()}
     # json.dump(r, open("{0}.json".format(line), "w"), indent=1)
 
 def create_table(curs):
@@ -71,56 +71,58 @@ def create_table(curs):
             """)
 
 if __name__ == "__main__":
-    conn_str = "host=localhost dbname=postgres user=postgres password=dilectic"
-    conn = psycopg2.connect(conn_str)
-    curs = conn.cursor()
-    create_table(curs)
-    old_predictions = {}
-    line_id = "88"
-    while True:
-        print("Fetching")
-        predictions = defaultdict(lambda: defaultdict(list))
-        arrivals = []
-        for prediction in fetch_arrival_predictions(line_id):
-            vehicle_id = prediction["vehicleId"]
-            naptan_id = prediction["naptanId"]
-            predictions[vehicle_id][naptan_id] = {
-                "direction": prediction["direction"],
-                "expectedArrival": prediction["expectedArrival"],
-                "timestamp": prediction["timestamp"],
-                "stationName": prediction["stationName"]
-            }
-        for vehicle_id in old_predictions.keys():
-            if not vehicle_id in predictions:
-                station_name = list(old_predictions[vehicle_id].keys())[0]
-                arrival = old_predictions[vehicle_id][naptan_id]
-                arrivals.append({
-                    "lineId": line_id,
-                    "vehicleId": vehicle_id,
-                    "naptanId": naptan_id,
-                    "stationName": arrival["stationName"],
-                    "direction": arrival["direction"],
-                    "lastExpectedArrival": arrival["expectedArrival"],
-                    "lastTimestamp": arrival["timestamp"]
-                })
-            else:
-                for naptan_id in old_predictions[vehicle_id]:
-                    if not naptan_id in predictions[vehicle_id]:
-                        arrival = old_predictions[vehicle_id][naptan_id]
-                        arrivals.append({
-                        "lineId": line_id,
-                        "vehicleId": vehicle_id,
-                        "naptanId": naptan_id,
-                        "stationName": arrival["stationName"],
-                        "direction": arrival["direction"],
-                        "lastExpectedArrival": arrival["expectedArrival"],
-                        "lastTimestamp": arrival["timestamp"]
-                        })
-
-        for arrival in arrivals:
-            pprint(arrival)
-        old_predictions = predictions
-            # curs.execute("INSERT INTO tfl_arrivals(lineId, vehicleId, towards, direction, naptanId, ts, expectedArrival ) VALUES(%s, %s, %s, %s, %s, %s, %s)",
-            #     (bus["lineId"], bus["vehicleId"], bus["towards"], bus["direction"], bus["naptanId"], bus["timestamp"], bus["expectedArrival"]))
-        conn.commit()
-        time.sleep(2)
+    test = fetch_arrival_predictions('88')
+    pprint(test)
+    # conn_str = "host=localhost dbname=postgres user=postgres password=dilectic"
+    # conn = psycopg2.connect(conn_str)
+    # curs = conn.cursor()
+    # create_table(curs)
+    # old_predictions = {}
+    # line_id = "88"
+    # while True:
+    #     print("Fetching")
+    #     predictions = defaultdict(lambda: defaultdict(list))
+    #     arrivals = []
+    #     for prediction in fetch_arrival_predictions(line_id):
+    #         vehicle_id = prediction["vehicleId"]
+    #         naptan_id = prediction["naptanId"]
+    #         predictions[vehicle_id][naptan_id] = {
+    #             "direction": prediction["direction"],
+    #             "expectedArrival": prediction["expectedArrival"],
+    #             "timestamp": prediction["timestamp"],
+    #             "stationName": prediction["stationName"]
+    #         }
+    #     for vehicle_id in old_predictions.keys():
+    #         if not vehicle_id in predictions:
+    #             station_name = list(old_predictions[vehicle_id].keys())[0]
+    #             arrival = old_predictions[vehicle_id][naptan_id]
+    #             arrivals.append({
+    #                 "lineId": line_id,
+    #                 "vehicleId": vehicle_id,
+    #                 "naptanId": naptan_id,
+    #                 "stationName": arrival["stationName"],
+    #                 "direction": arrival["direction"],
+    #                 "lastExpectedArrival": arrival["expectedArrival"],
+    #                 "lastTimestamp": arrival["timestamp"]
+    #             })
+    #         else:
+    #             for naptan_id in old_predictions[vehicle_id]:
+    #                 if not naptan_id in predictions[vehicle_id]:
+    #                     arrival = old_predictions[vehicle_id][naptan_id]
+    #                     arrivals.append({
+    #                     "lineId": line_id,
+    #                     "vehicleId": vehicle_id,
+    #                     "naptanId": naptan_id,
+    #                     "stationName": arrival["stationName"],
+    #                     "direction": arrival["direction"],
+    #                     "lastExpectedArrival": arrival["expectedArrival"],
+    #                     "lastTimestamp": arrival["timestamp"]
+    #                     })
+    #
+    #     for arrival in arrivals:
+    #         pprint(arrival)
+    #     old_predictions = predictions
+    #         # curs.execute("INSERT INTO tfl_arrivals(lineId, vehicleId, towards, direction, naptanId, ts, expectedArrival ) VALUES(%s, %s, %s, %s, %s, %s, %s)",
+    #         #     (bus["lineId"], bus["vehicleId"], bus["towards"], bus["direction"], bus["naptanId"], bus["timestamp"], bus["expectedArrival"]))
+    #     conn.commit()
+    #     time.sleep(2)
