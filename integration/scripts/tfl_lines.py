158df295babd8b4ff84dc19b58627c084d5088a7
Oliver Charlesworth <oliver@quartic.io>
2016-10-18 11:23:47 +0100

    Rename things sensibly, and minimise number of API calls

--- integration/scripts/tfl_lines.py
+++ integration/scripts/tfl_lines.py
@@ -64,75 +64,70 @@ def lookup_line(line_id):
 def fetch_arrival_predictions(line):
     bus_arrivals = {}
     r = request("/line/{0}/arrivals".format(line))
-    for bus in r:
-        if bus['vehicleId'] in bus_arrivals.keys():
-            bus_arrivals[bus['vehicleId']].append(bus)
+    for arrival in r:
+        bus_id = arrival['vehicleId']
+        if bus_id in bus_arrivals.keys():
+            bus_arrivals[bus_id].append(arrival)
         else:
-            bus_arrivals[bus['vehicleId']] = [bus]
-    return {bus: sorted(arrivals, key=lambda k: k['timeToStation']) for bus, arrivals in bus_arrivals.items()}
-    # json.dump(r, open("{0}.json".format(line), "w"), indent=1)
+            bus_arrivals[bus_id] = [arrival]
+    return {bus_id: min(arrivals, key=lambda k: k['timeToStation']) for bus_id, arrivals in bus_arrivals.items()}
 
-def previous_stop(bus, line_info):
-    next_stop = bus['naptanId']
-    line_direction = line_info[bus['direction']]
+def previous_stop(bus_arrival, line_info):
+    next_stop = bus_arrival['naptanId']
+    line_direction = line_info[bus_arrival['direction']]
     next_index = list(line_direction.keys()).index(next_stop)
     return list(line_direction.values())[next_index-1]
 
-def current_stop(bus, line_info):
-    current_stop = bus['naptanId']
-    line_direction = line_info[bus['direction']]
+def current_stop(bus_arrival, line_info):
+    current_stop = bus_arrival['naptanId']
+    line_direction = line_info[bus_arrival['direction']]
     return line_direction[current_stop]
 
-def prepare_geojson(bus, pos):
-    bus_feature = geojson.Feature(id=bus, geometry=pos,
-        properties={'registration':bus})
+def prepare_geojson(bus_id, pos):
+    bus_feature = geojson.Feature(id=bus_id, geometry=pos, properties={'registration':bus_id})
     return bus_feature
 
 def get_position(current, next_pos, proportion):
     segment = LineString(((current[2], current[1]), (next_pos[2], next_pos[1])))
     return segment.interpolate(distance=proportion, normalized=True)
 
-def time_to_station(line, time_to_dest):
-    buses = fetch_arrival_predictions(line)
-    for bus, bus_deets in buses.items():
-        if bus in time_to_dest.keys():
-            if time_to_dest[bus] <= bus_deets[0]['timeToStation']:
-                time_to_dest[bus] = bus_deets[0]['timeToStation']
+def time_to_station(bus_arrivals, time_to_dest):
+    for bus_id, bus_arrival in bus_arrivals.items():
+        if bus_id in time_to_dest.keys():
+            if time_to_dest[bus_id] <= bus_arrival['timeToStation']:
+                time_to_dest[bus_id] = bus_arrival['timeToStation']
             else:
                 continue
         else:
-            time_to_dest[bus] = bus_deets[0]['timeToStation']
+            time_to_dest[bus_id] = bus_arrival['timeToStation']
     return time_to_dest
 
-def estimate_to_station(line, eta, dt):
-    buses = fetch_arrival_predictions(line)
-    for bus, bus_deets in buses.items():
-        if bus in eta.keys():
-            if eta[bus] > bus_deets[0]['timeToStation']:
-                eta[bus] = bus_deets[0]['timeToStation']
+def estimate_to_station(bus_arrivals, eta, dt):
+    for bus_id, bus_arrival in bus_arrivals.items():
+        if bus_id in eta.keys():
+            if eta[bus_id] > bus_arrival['timeToStation']:
+                eta[bus_id] = bus_arrival['timeToStation']
             else:
-                if eta[bus] > dt:
-                    eta[bus] = eta[bus] - dt
+                if eta[bus_id] > dt:
+                    eta[bus_id] = eta[bus_id] - dt
                 else:
-                    eta[bus] = 0
+                    eta[bus_id] = 0
         else:
-            eta[bus] = bus_deets[0]['timeToStation']
+            eta[bus_id] = bus_arrival['timeToStation']
     return eta
 
 
-def prepare_event(line, time_to_dest, eta, path):
-    buses = fetch_arrival_predictions(line)
+def prepare_event(line_info, bus_arrivals, time_to_dest, eta, path):
     bus_place = {}
-    line_info = lookup_line(line)
     collection = []
-    for bus, bus_stops in buses.items():
+    for bus_id, bus_arrival in bus_arrivals.items():
         try:
-            previous = previous_stop(bus_stops[0], line_info)
-            current = current_stop(bus_stops[0], line_info)
-            proportion = eta[bus]/time_to_dest[bus]
+            previous = previous_stop(bus_arrival, line_info)
+            current = current_stop(bus_arrival, line_info)
+            proportion = eta[bus_id] / time_to_dest[bus_id]
             pos = get_position(current, previous, proportion)
             # pos = path.interpolate(path.project(pos, normalized=True))#attempt to get it on the line
-            collection.append(prepare_geojson(bus, pos))
+            collection.append(prepare_geojson(bus_id, pos))
         except Exception as e:
             print(e)
 
@@ -148,12 +143,15 @@ def prepare_event(line, time_to_dest, eta, path):
 if __name__ == "__main__":
     LINE_ID = '88'
 
+    line_info = lookup_line(LINE_ID)
     path = lookup_line_path(LINE_ID, 'inbound')
     time_to_dest = {}#tracks total time to next dest
     eta = {}#tracks estimated time to next dest
     dt=5
     while True:
-        time_to_dest = time_to_station(LINE_ID, time_to_dest)
-        eta = estimate_to_station(LINE_ID, eta, dt)
-        prepare_event(LINE_ID, time_to_dest, eta, path)
+        bus_arrivals = fetch_arrival_predictions(LINE_ID)
+        time_to_dest = time_to_station(bus_arrivals, time_to_dest)
+        eta = estimate_to_station(bus_arrivals, eta, dt)
+        prepare_event(line_info, bus_arrivals, time_to_dest, eta, path)
+
         time.sleep(dt)
