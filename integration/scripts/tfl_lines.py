5842e91552871eb6ae1ac6f9d0d365e1d9b436e5
Arlo Bryer <arlo@quartic.io>
2016-10-18 01:01:34 +0100

    Buses moving along lines mostly regularly.

--- integration/scripts/tfl_lines.py
+++ integration/scripts/tfl_lines.py
@@ -8,9 +8,13 @@ from collections import defaultdict, OrderedDict
 import geojson
 import datetime
 import utils
+import shapely.geometry as SG
+from shapely.geometry import LineString
 
-APP_ID="4abd99df"
-APP_KEY="0f76ba70a21836b0991d192dceae511b"
+# APP_ID="4abd99df"
+# APP_KEY="0f76ba70a21836b0991d192dceae511b"
+APP_ID = "860e7675"
+APP_KEY = "1d36a20279e6ac727ddfdcaeba2e97ea"
 
 FIELDS = [
         "stationName",
@@ -68,12 +72,49 @@ def previous_stop(bus, line_info):
     next_index = list(line_direction.keys()).index(next_stop)
     return list(line_direction.values())[next_index-1]
 
-def prepare_geojson(bus, stop):
-    loc = geojson.Point((stop[2], stop[1]))
-    bus_feature = geojson.Feature(id=bus, geometry=loc, properties=None)
+def current_stop(bus, line_info):
+    current_stop = bus['naptanId']
+    line_direction = line_info[bus['direction']]
+    return line_direction[current_stop]
+
+def prepare_geojson(bus, pos):
+    bus_feature = geojson.Feature(id=bus, geometry=pos,
+        properties={'registration':bus})
     return bus_feature
 
-def prepare_event(line):
+def get_position(current, next_pos, proportion):
+    segment = LineString(((current[2], current[1]), (next_pos[2], next_pos[1])))
+    return segment.interpolate(distance=proportion, normalized=True)
+
+def time_to_station(line, time_to_dest):
+    buses = fetch_arrival_predictions(line)
+    for bus, bus_deets in buses.items():
+        if bus in time_to_dest.keys():
+            if time_to_dest[bus] <= bus_deets[0]['timeToStation']:
+                time_to_dest[bus] = bus_deets[0]['timeToStation']
+            else:
+                continue
+        else:
+            time_to_dest[bus] = bus_deets[0]['timeToStation']
+    return time_to_dest
+
+def estimate_to_station(line, eta, dt):
+    buses = fetch_arrival_predictions(line)
+    for bus, bus_deets in buses.items():
+        if bus in eta.keys():
+            if eta[bus] > bus_deets[0]['timeToStation']:
+                eta[bus] = bus_deets[0]['timeToStation']
+            else:
+                if eta[bus] > dt:
+                    eta[bus] = eta[bus] - dt
+                else:
+                    eta[bus] = 0
+        else:
+            eta[bus] = bus_deets[0]['timeToStation']
+    return eta
+
+
+def prepare_event(line, time_to_dest, eta):
     buses = fetch_arrival_predictions(line)
     bus_place = {}
     line_info = lookup_line(line)
@@ -81,19 +122,20 @@ def prepare_event(line):
     for bus, bus_stops in buses.items():
         try:
             previous = previous_stop(bus_stops[0], line_info)
-            collection.append(prepare_geojson(bus, previous))
+            current = current_stop(bus_stops[0], line_info)
+            proportion = eta[bus]/time_to_dest[bus]
+            pos = get_position(current, previous, proportion)
+            collection.append(prepare_geojson(bus, pos))
         except Exception as e:
             print(e)
     e = {'name' : "Buses",
         'description' : "buses",
         'icon' : 'bus',
+        'attribution' : 'TfL',
         'viewType' : 'LOCATION_AND_TRACK',
         'events' : [{'timestamp' : 0,
                     'featureCollection' : geojson.FeatureCollection(collection)}]}
     utils.post_events('buses', e, 'http://localhost:8080/api')
-
-
-
 
 def create_table(curs):
     curs.execute("""
@@ -107,7 +149,14 @@ def create_table(curs):
             """)
 
 if __name__ == "__main__":
-    test = prepare_event('88')
+    time_to_dest = {}#tracks total time to next dest
+    eta = {}#tracks estimated time to next dest
+    dt=5
+    while True:
+        time_to_dest = time_to_station('88', time_to_dest)
+        eta = estimate_to_station('88', eta, dt)
+        prepare_event('88', time_to_dest, eta)
+        time.sleep(dt)
     # conn_str = "host=localhost dbname=postgres user=postgres password=dilectic"
     # conn = psycopg2.connect(conn_str)
     # curs = conn.cursor()
