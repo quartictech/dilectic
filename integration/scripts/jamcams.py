4feb2a2ecabb038c7d50beb6259a620efc9b3857
Oliver Charlesworth <oliver@quartic.io>
2016-10-19 10:16:31 +0100

    Switch to MP4 for jamcams

--- integration/scripts/jamcams.py
+++ integration/scripts/jamcams.py
@@ -4,6 +4,7 @@ from utils import parse_date
 
 
 def fill_jamcams(data_dir):
+    # TODO: we should switch to https://api.tfl.gov.uk/Place/Type/jamcam
     r = requests.get('https://s3-eu-west-1.amazonaws.com/jamcams.tfl.gov.uk/jamcams-camera-list.xml')
     xml = r.text
     root = ElementTree.fromstring(xml)
@@ -12,6 +13,6 @@ def fill_jamcams(data_dir):
         v = []
         for t in c.itertext():
             v.append(t)
-        values = (v[0], v[1], v[2], 'https://s3-eu-west-1.amazonaws.com/jamcams.tfl.gov.uk/{}'.format(v[3]),
+        values = (v[0], v[1], v[2], 'https://s3-eu-west-1.amazonaws.com/jamcams.tfl.gov.uk/{}'.format(v[3].replace("jpg", "mp4")),
                 parse_date(v[4], fmt='%Y-%m-%dT%H:%M:%SZ'), v[7], v[8], v[10])
         yield values
