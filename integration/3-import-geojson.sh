54060f818e9776fb84bf868398a371520b091009
Alex Sparrow <alex@quartic.io>
2016-10-20 09:17:23 +0100

    Road traffic counts data

--- integration/3-import-geojson.sh
+++ integration/3-import-geojson.sh
@@ -2,3 +2,4 @@
 pip3 install -r /home/integration/requirements.txt
 
 python3 /home/integration/scripts/billboards.py /home/data/raw/signkick.csv /home/data/derived/signkick.json
+python3 /home/integration/scripts/gb_road_traffic_counts.py /home/data/derived/gb-road-traffic-counts/AADF-data-major-roads.csv /home/data/derived/gb-road-traffic-counts/AADF-data-major-roads.json
