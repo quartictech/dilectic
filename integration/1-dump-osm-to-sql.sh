2b432fde0fe518dfde050c494af79915d14f08f4
Oliver Charlesworth <oliver@quartic.io>
2016-10-22 20:40:04 +0100

    Add ON_ERROR_STOP

--- integration/1-dump-osm-to-sql.sh
+++ integration/1-dump-osm-to-sql.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/config.sh
 
 osm2pgsql $INPUT/greater-london-latest.osm.pbf -d postgres -U postgres -P 5432 -H localhost
-psql -d postgres -U postgres -a -f $DIR/scripts/rename_columns.sql -h localhost
+psql -v ON_ERROR_STOP=1 -d postgres -U postgres -a -f $DIR/scripts/rename_columns.sql -h localhost
