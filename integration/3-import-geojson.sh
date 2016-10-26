79d178bd8000cba533203f24201da1a915b5c5cc
Alex Sparrow <alex@quartic.io>
2016-10-26 23:32:37 +0000

    Make dir before running integration script

--- integration/3-import-geojson.sh
+++ integration/3-import-geojson.sh
@@ -2,4 +2,5 @@
 pip3 install -r /home/integration/requirements.txt
 
 python3 /home/integration/scripts/billboards.py /home/data/raw/signkick.csv /home/data/derived/signkick.json
+mkdir /home/data/derived/gb-road-traffic-counts
 python3 /home/integration/scripts/gb_road_traffic_counts.py /home/data/derived/gb-road-traffic-counts/AADF-data-major-roads.csv /home/data/derived/gb-road-traffic-counts/AADF-data-major-roads.json
