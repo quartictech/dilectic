1db4df143d93c4d236dd14f1d9914f03027b6ed2
Oliver Charlesworth <oliver@quartic.io>
2016-11-24 21:49:47 +0000

    Bump data-worker version for gdal

--- integration/config.sh
+++ integration/config.sh
@@ -1,3 +1,3 @@
 export QUARTIC_DOCKER_REPOSITORY=eu.gcr.io/quartictech
-export DOCKER_IMAGE=${QUARTIC_DOCKER_REPOSITORY}/data-worker:0.2.0-43-ga2e4ce4
+export DOCKER_IMAGE=${QUARTIC_DOCKER_REPOSITORY}/data-worker:0.3.0-3-g001356a
 export PGPASSWORD=dilectic
