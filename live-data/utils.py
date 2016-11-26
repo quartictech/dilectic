6480365d39d447061ab80e474179287a4238e9cb
Oliver Charlesworth <oliver@quartic.io>
2016-11-26 11:29:26 +0000

    Delete unused

--- live-data/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import requests
-import sys
-
-def post_events(layer, data, API_ROOT='http://localhost:8080/api'):
-    r = requests.post("{}/layer/live/{}".format(API_ROOT, layer), json=data)
-    if (r.status_code < 200 or r.status_code > 299):
-        print(r.text)
